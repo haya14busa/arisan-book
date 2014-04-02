b5eabad76072d2d9f449e21603a6cc5e117d11df
haya14busa <hayabusa1419@gmail.com>
2014-04-02 21:41:47 +0900

    Improve lottery in python

--- Chapter1/001-lottery.py
+++ Chapter1/001-lottery.py
@@ -29,6 +29,7 @@ MAX_N = 50
 
 
 def four_loop(n, m, k):
+    """O(n^4)"""
     # n: the number of lots
     # m: the expected sum
     # k: choosed_list contains 4 numbers
@@ -40,11 +41,54 @@ def four_loop(n, m, k):
                         return True
     return False
 
+def binary_search(sorted_list, query):
+    """O(logn)"""
+    def binary_helper(begin, end):
+        if begin == end and not sorted_list[begin] == query:
+            return False
+        middle = int((begin + end) / 2)
+        if sorted_list[middle] == query:
+            return True
+        elif sorted_list[middle] < query:
+            return binary_helper(middle + 1, end)
+        else:
+            return binary_helper(begin, middle)
+    return binary_helper(0, len(sorted_list) - 1)
+
+
+def improved_lottery(n, m, k):
+    """O(n^2logn)"""
+    powered_list = [k[i] + k[j] for i in range(n) for j in range(n)]
+    # Sort for binary search
+    powered_list.sort()
+    for i in range(n):
+        for j in range(n):
+            if binary_search(powered_list, m - k[i] - k[j]):
+                return True
+    return False
+
 
 def main():
     assert four_loop(3, 10, [1, 3, 5]) == True
     assert four_loop(3, 9, [1, 3, 5]) == False
     assert four_loop(5, 18, [1, 3, 5, 9, 10]) == True
+
+
+    # binary search
+    assert binary_search([1,3,5,6,7,10], 1) == True
+    assert binary_search([1,3,5,6,7,10], 5) == True
+    assert binary_search([1,3,5,6,7,10], 10) == True
+    assert binary_search([1,3,5,6,7,10], 11) == False
+    assert binary_search([1,3,5,6,7,10], 2) == False
+    assert binary_search([1,3,5,6,7,10], -10000) == False
+
+    # Improve
+    assert improved_lottery(3, 10, [1, 3, 5]) == True
+    assert improved_lottery(3, 9, [1, 3, 5]) == False
+    assert improved_lottery(5, 18, [1, 3, 5, 9, 10]) == True
+    assert improved_lottery(5, 18, [10, 3, 5, 9, 1]) == True
+    assert improved_lottery(5, 18, [10, 11, 9, 300, 1]) == False
+
     print('All done!')
 
 if __name__ == '__main__':
