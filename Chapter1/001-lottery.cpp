79d98ba129a3b9ee6b979db4a3f1fc857a8617bf
haya14busa <hayabusa1419@gmail.com>
2014-04-02 17:51:57 +0900

    Implement binary_search() in 001-lottery in c++

--- Chapter1/001-lottery.cpp
+++ Chapter1/001-lottery.cpp
@@ -1,5 +1,9 @@
 #include <cstdio>
 #include <assert.h>
+#include <algorithm>
+#include <deque>
+#include <iostream>
+#include <list>
 
 const int MAX_N = 50;
 
@@ -27,6 +31,22 @@ int four_loop(int n, int m, int k[MAX_N]) {
     return flag;
 }
 
+bool binary_search(int begin, int end, int array[], int x) {
+    // The number of element is 1 && equals to x
+    if (begin == end && array[begin] != x) return false;
+
+    // binary_search
+    int i = (begin + end) / 2;
+    if (array[i] == x) {
+        return true;
+    } else if (array[i] < x) {
+        // recursive call
+        return binary_search(i+1, end, array, x);
+    } else {
+        return binary_search(begin, i, array, x);
+    }
+}
+
 int main() {
     // n: the number of lot
     // m: the sum of 4 cards
@@ -44,9 +64,25 @@ int main() {
     int m3 = 18;
     int k3[] = {1, 3, 5, 9, 10}; // -> {1, 3, 5, 9}
 
+    // 4 Loop
     assert(four_loop(n1, m1, k1) == 1);
     assert(four_loop(n2, m2, k2) == 0);
     assert(four_loop(n3, m3, k3) == 1);
+
+    // Binary-search
+    assert(binary_search(0, 2, k1, -1) == false);
+    assert(binary_search(0, 2, k1, 1) == true);
+    assert(binary_search(0, 2, k1, 3) == true);
+    assert(binary_search(0, 2, k1, 5) == true);
+    assert(binary_search(0, 2, k1, 9) == false);
+    assert(binary_search(0, 2, k1, 300) == false);
+    int list[4] = {1,2,3,4};
+    assert(binary_search(0, 3, list, 1) == true);
+    assert(binary_search(0, 3, list, 2) == true);
+    assert(binary_search(0, 3, list, 3) == true);
+    assert(binary_search(0, 3, list, 4) == true);
+    assert(binary_search(0, 3, list, 5) == false);
+    assert(binary_search(0, 3, list, 50000) == false);
 
     puts("All done!");
     return 0;
